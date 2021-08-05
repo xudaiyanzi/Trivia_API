6678fc5271a86a6527f571f13eb94acb2619bfd1
Yan Xu <xudaiyanzi@gmail.com>
2021-08-05 12:44:18 -0500

    update the formatting of database

--- backend/models.py
+++ backend/models.py
@@ -3,8 +3,9 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_name = os.getenv('DB_NAME','trivia')
+database_host = os.getenv('DB_HOST', 'localhost:5432')
+database_path = "postgres://{}/{}".format(database_host, database_name)
 
 db = SQLAlchemy()
 
